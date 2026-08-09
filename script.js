7b586b80d0577b0d1618a7f195515a59e0d73a92
zzxxzz-bit <johnwicekk@gmail.com>
2026-08-09 04:00:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ const TICKER_PHRASES = [
   "🎭 only in ohio ce genre de move",
   "⚡ tu deviens littéralement une légende sigma",
   "🥶 trop froid, même le rizz gèle",
+   "xilio c un pseudo de pedo",
   "🫠 splortch approuve ton grind sans limite",
 ];
 
